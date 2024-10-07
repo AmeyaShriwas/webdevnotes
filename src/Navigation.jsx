d3fc3db4fe2a1de0d560881881eb267e875fa58b
AmeyaShriwas133 <shriwas.ameya@webseeder.in>
2024-10-07 20:12:22 +0530

    404

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Landing from './Pages/Landing/Landing';
 import AuthForm from './Components/Form/Form';
 import NotesPage from './Pages/NotesPage/NotesPage';
@@ -7,14 +7,7 @@ import CartPage from './Pages/CartPage/CartPage';
 import NotFoundPage from './Pages/PageNotFound/PageNotFound';
 import ResetPassword from './Components/ResetPassword/ResetPassword';
 
-
-
-
 const Navigation = () => {
-
-  
-
-
   return (
     <Router>
       <Routes>
@@ -23,10 +16,9 @@ const Navigation = () => {
         <Route path="/notes" element={<NotesPage />} />
         <Route path="/cart" element={<CartPage />} />
         <Route path="/resetPassword" element={<ResetPassword />} />
-        <Route path="*" element={<NotFoundPage/>} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Router>
-   
   );
 };
 

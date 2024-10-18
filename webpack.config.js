a48ba798eadf74457110d4d037a7f21edeffe335
AmeyaShriwas133 <shriwas.ameya@webseeder.in>
2024-10-18 19:11:19 +0530

    new

--- webpack.config.js
+++ webpack.config.js
@@ -6,26 +6,25 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
-  mode: 'production', // or 'development'
   module: {
     rules: [
       {
-        test: /\.js$/, // This applies to .js and .jsx files
+        test: /\.(js|jsx)$/, // Handle .js and .jsx files
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
+            presets: ['@babel/preset-env', '@babel/preset-react'], // Use these Babel presets
           },
         },
       },
       {
-        test: /\.css$/, // If you have CSS imports
+        test: /\.css$/, // Handle CSS imports
         use: ['style-loader', 'css-loader'],
       },
     ],
   },
   resolve: {
-    extensions: ['.js', '.jsx'], // Allows imports without specifying file extensions
+    extensions: ['.js', '.jsx'], // Allow omitting .js and .jsx extensions when importing
   },
 };

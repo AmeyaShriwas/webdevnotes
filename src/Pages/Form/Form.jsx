af216cd0f0034c266e50700af50a7a4a96620fdd
AmeyaShriwas133 <shriwas.ameya@webseeder.in>
2024-10-19 14:26:15 +0530

    new

--- src/Pages/Form/Form.jsx
+++ src/Pages/Form/Form.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import AuthForm from '../../Components/Form/Form'
-import Header from '../../Components/Header/Header'
+// import Header from '../../Components/Header/Header'
 
 const Form = () => {
   return (
     <>
-    <Header/>
+    {/* <Header/> */}
     <AuthForm/>
     </>
   )

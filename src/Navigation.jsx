c49775ed35b9ae4521970d56a7c8d10bf8090fc0
AmeyaShriwas133 <shriwas.ameya@webseeder.in>
2024-10-19 22:16:38 +0530

    new

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// Lazy load your components
 const Landing = React.lazy(() => import('./Pages/Landing/Landing'));
 const AuthForm = React.lazy(() => import('./Components/Form/Form'));
 const NotesPage = React.lazy(() => import('./Pages/NotesPage/NotesPage'));
@@ -14,24 +16,28 @@ const TermsAndConditions = React.lazy(() => import('./Pages/TermsAndConditions/T
 const ScrollToTop = React.lazy(() => import('./Components/ScrollToTop/ScrollToTop'));
 const ProfilePage = React.lazy(() => import('./Pages/ProfilePage/ProfilePage'));
 
+const Loading = () => <div>Loading...</div>; // Loading component for lazy loading
+
 const Navigation = () => {
   return (
     <Router>
-      <ScrollToTop/>
-      <Routes>
-        <Route path="/" element={<AuthForm />} />
-        <Route path="/landing" element={<Landing />} />
-        <Route path="/notes" element={<NotesPage />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/resetPassword" element={<ResetPassword />} />
-        <Route path="/terms" element={<TermsAndConditions />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-        <Route path="/refund-policy" element={<RefundPolicy />} />
-        <Route path="/shipping-policy" element={<ShippingPolicy />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <Suspense fallback={<Loading />}>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<AuthForm />} />
+          <Route path="/landing" element={<Landing />} />
+          <Route path="/notes" element={<NotesPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/resetPassword" element={<ResetPassword />} />
+          <Route path="/terms" element={<TermsAndConditions />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/refund-policy" element={<RefundPolicy />} />
+          <Route path="/shipping-policy" element={<ShippingPolicy />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 };

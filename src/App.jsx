5fca29b55a9efe39cb8922093cae78ec95144c2b
AmeyaShriwas133 <shriwas.ameya@webseeder.in>
2024-10-21 14:23:36 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,19 @@ import React from 'react';
 import Navigation from './Navigation';
 import store from './redux/store';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistStore } from 'redux-persist';
+
 import { ToastContainer } from 'react-toastify';
 
 // Create the persistor
-let persistor = persistStore(store);
 
 const App = () => {
   return (
     <Provider store={store}>
       {/* PersistGate delays rendering until the persisted state is loaded */}
-      <PersistGate loading={null} persistor={persistor}>
+    
         <Navigation />
         <ToastContainer/>
-      </PersistGate>
+     
     </Provider>
   );
 };
